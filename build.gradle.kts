1c6f1a88dff2bff72160ba5a9e8e24c5f3d74a7a
Arthur588 <jab86820@gmail.com>
2025-12-27 16:40:10 +0200

    Update Kotlin Android plugin version to 1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 plugins {
-    // Apply the Android Application plugin and Kotlin Android plugin in the app module
     id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
